84445111495021ebdb8c2cd21e1f0588f4d15580
Abairaj <abai.raj@scrapehero.com>
2024-09-01 15:23:16 +0530

    changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,9 @@ import { useState, useEffect } from "react";
 import "./App.css";
 import { CreateTodo } from "./components/CreateTodo";
 import { Todos } from "./components/Todos";
-import { Space } from "antd";
+import Layout, { Header } from "antd/es/layout/layout";
+import Sider from "antd/es/layout/Sider";
+
 function App() {
   const [todos, setTodos] = useState([]);
 
@@ -14,12 +16,25 @@ function App() {
   });
 
   return (
-    <div className="main-div">
-      <div>
-      <Todos toDos={todos} />
-      <CreateTodo />
-      </div>
-    </div>
+    <Layout style={{ minHeight: "100vh", backgroundColor: "#2f3136" }}>
+      <Sider width="200px" style={{ backgroundColor: "#23272a" }}>
+        <div className="logo" />
+      </Sider>
+      <Layout>
+        <Header
+          style={{
+            backgroundColor: "#23272a",
+            color: "#fff",
+            padding: 0,
+            fontSize: "25px",
+          }}
+        >
+          Todo App
+        </Header>
+        <Todos toDos={todos} />
+        <CreateTodo />
+      </Layout>
+    </Layout>
   );
 }
 
--- frontend/src/components/Todos.jsx
+++ frontend/src/components/Todos.jsx
@@ -1,5 +1,6 @@
-import { Button, Flex, Typography, Checkbox } from "antd";
+import { Button, Flex, Typography, Checkbox, Space } from "antd";
 import { DeleteOutlined } from "@ant-design/icons";
+import { Content } from "antd/es/layout/layout";
 const { Title } = Typography;
 
 export function Todos({ toDos }) {
@@ -7,53 +8,55 @@ export function Todos({ toDos }) {
     e.target.checked = !e.target.checked;
   };
   return (
-    <>
-      {toDos.map((todo) => {
-        return (
-          <div key={todo._id} className="todos-main-block">
-            <div className="todo-text">
-              <Title level={2}>{todo.title}</Title>
-              <Title level={3}>{todo.description}</Title>
+    <Content style={{ padding: "0 24px", minHeight: 280 }}>
+      <Space direction="vertical" size="large" style={{ width: "100%" }}>
+        {toDos.map((todo) => {
+          return (
+            <div key={todo._id} className="todos-main-block">
+              <div className="todo-text">
+                <Title level={2}>{todo.title}</Title>
+                <Title level={3}>{todo.description}</Title>
+              </div>
+              <div className="todo-actions">
+                <Checkbox
+                  onClick={(e) => {
+                    fetch("http://localhost:3000/completed", {
+                      method: "PUT",
+                      body: JSON.stringify({
+                        id: todo._id,
+                        completed: !todo.completed,
+                      }),
+                      headers: { "content-type": "application/json" },
+                    }).then(async (response) => {
+                      const json_data = await response.json();
+                      e.target.value = !e.target.value;
+                    });
+                  }}
+                  size="small"
+                  checked={todo.completed}
+                  onChange={onChange}
+                ></Checkbox>
+                <Button
+                  onClick={() => {
+                    fetch(`http://localhost:3000/todo/${todo._id}`, {
+                      method: "DELETE",
+                    }).then(async (response) => {
+                      const json_data = await response.json();
+                      alert("Deleted todo successfully");
+                    });
+                  }}
+                  size="small"
+                  style={{ margin: "0 6px" }}
+                  type="primary"
+                  danger
+                >
+                  <DeleteOutlined />
+                </Button>
+              </div>
             </div>
-            <div className="todo-actions">
-              <Checkbox
-                onClick={(e) => {
-                  fetch("http://localhost:3000/completed", {
-                    method: "PUT",
-                    body: JSON.stringify({
-                      id: todo._id,
-                      completed: !todo.completed,
-                    }),
-                    headers: { "content-type": "application/json" },
-                  }).then(async (response) => {
-                    const json_data = await response.json();
-                    e.target.value = !e.target.value;
-                  });
-                }}
-                size="small"
-                checked={todo.completed}
-                onChange={onChange}
-              ></Checkbox>
-              <Button
-                onClick={() => {
-                  fetch(`http://localhost:3000/todo/${todo._id}`, {
-                    method: "DELETE",
-                  }).then(async (response) => {
-                    const json_data = await response.json();
-                    alert("Deleted todo successfully");
-                  });
-                }}
-                size="small"
-                style={{ margin: "0 6px" }}
-                type="primary"
-                danger
-              >
-                <DeleteOutlined />
-              </Button>
-            </div>
-          </div>
-        );
-      })}
-    </>
+          );
+        })}
+      </Space>
+    </Content>
   );
 }
